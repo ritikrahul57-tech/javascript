49ca274b9ce54c8c74e6376c453562a04d524fc7
ritikrahul57-tech <ritikrahul57@gmail.com>
2026-07-22 21:58:03 +0530

    The python day 1

--- section1.js
+++ section1.js
@@ -1,11 +1,1 @@
                   // The part one of java
-
-
-let num1 = 10;
-let num2 = 20;
-
-let sum = num1 + num2;
-
-console.log("Number 1:", num1);
-console.log("Number 2:", num2);
-console.log("Sum =", sum);
