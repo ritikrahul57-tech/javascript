ae4c4df744dbeec4ce4cf0b303bccb30213fdc65
ritikrahul57-tech <ritikrahul57@gmail.com>
2026-07-22 22:23:04 +0530

    The python day 1

--- section1.js
+++ section1.js
@@ -1,1 +1,15 @@
                   // The part one of java
+
+
+                  // A simple program that calculates the factorial of a number
+
+function factorial(n) {
+  if (n === 0 || n === 1) {
+    return 1;
+  }
+  return n * factorial(n - 1);
+}
+
+// Try it out
+let num = 5;
+console.log(`Factorial of ${num} is ${factorial(num)}`);
