c791f908392e8759ad14bcd0f7f026e75bbdda77
ritikrahul57-tech <ritikrahul57@gmail.com>
2026-07-25 22:34:20 +0530

    The javascript day3

--- section1.js
+++ section1.js
@@ -12,20 +12,20 @@ alert("The new page")
 
 
      // variable declaration
-        let bookname, pageno;
-        let Bookname;
-        let book_price="60000"
+      //   let bookname, pageno;
+      //   let Bookname;
+      //   let book_price="60000"
      // variable initialization
-        bookname="one piece"
+      //   bookname="one piece"
 
 
 // let booknumber="1156";
 // let bookdate="july 5th"
 
-let  booknumber="1156"; bookdate="july 5th";
-bookdate="march 8th";
-bookname=booknumber;
-console.log(bookname, booknumber, "\t", bookdate);
+// let  booknumber="1156"; bookdate="july 5th";
+// bookdate="march 8th";
+// bookname=booknumber;
+// console.log(bookname, booknumber, "\t", bookdate);
 
 /* 
          Rules to name a variable
@@ -37,3 +37,38 @@ console.log(bookname, booknumber, "\t", bookdate);
  */
 
                    
+
+
+                  // Constant
+                        //  It cannot be changed
+
+
+
+// const value=sum(5-2);
+// const GMAIL="moonwargam.11";
+// const PASS="ritikrahul001";
+// const WHITE_COLOR="#fff";
+// const BLACK_COLOR="#000";
+// console.log(GMAIL,PASS);
+
+
+// const DATE_OF_BIRTH="5/7/2008";
+// const AGE=ca/age(DATE_OF_BIRTH)
+
+
+           
+
+                        //   Date type
+
+
+
+             /* There are 8 type of date type
+                   - number date type
+             */
+
+
+
+      // 1 . Number date type
+
+
+      
