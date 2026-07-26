af18b5b4fd14bc328065e507c3e0924addcbf4f5
ritikrahul57-tech <ritikrahul57@gmail.com>
2026-07-26 22:11:20 +0530

    The javascript day3

--- section2.js
+++ section2.js
@@ -2,12 +2,38 @@
 
 
 
-        // 2.  Big int
+          // 2.  Big int
 
 
 // 2*53-1=9007199254740991
 
-let limit=9007199254740991n;
-console.log(limit + 1n);
-console.log(limit + 2n);
-console.log(limit + 3n);
+// let limit=9007199254740991n;
+// console.log(limit + 1n);
+// console.log(limit + 2n);
+// console.log(limit + 3n);
+// console.log(typeof limit)
+
+
+// let periyanumber=12345678910n;
+// console.log( periyanumber + 2024n)
+
+
+
+         // 3.   string data type
+
+
+
+// let intro="My name is zoro 1+8";
+// let fahh="please don't touch the book"
+// let ooi='he said that "He loves anime"'
+// let uhh=`one piece's is real`
+// let embed=`1+2 ${1+2}`
+// let num=122-76
+// let ahh=${num}
+// console.log(typeof intro)
+// console.log(embed)
+
+
+// let name="Ritik rahul";
+// let embedd=`My name is ${name}`;
+// console.log(embedd)
