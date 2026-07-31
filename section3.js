00dc6db937621ca11dd540447d5149b92f163dbb
ritikrahul57-tech <ritikrahul57@gmail.com>
2026-07-31 21:51:13 +0530

    The javascript day4

--- section3.js
+++ section3.js
@@ -1,12 +1,7 @@
                 // Part 3 of javascript
 
 
-let name;
-name="Rahul"
-name=alert("You have a alert")
-name=prompt("What is your name")
-console.log(name)
 
-let output;
-output=confirm("is your name is rahuul")
-console.log(output)
+let name;
+name="My name is rahul"
+console.log(name)
