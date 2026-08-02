a5e38926ba0bd152a181d4521ecb4a2b411dc61d
ritikrahul57-tech <ritikrahul57@gmail.com>
2026-08-02 21:52:55 +0530

    The javascript day3

--- section3.js
+++ section3.js
@@ -1,11 +1,5 @@
                 // Part 3 of javascript
 
 
-
-let input;
-input="hello world"
-console.log(input)
-
-let output;
-output=prompt("Enter your name ")
-console.log(output)
+let name="ritik"
+console.log(name)
