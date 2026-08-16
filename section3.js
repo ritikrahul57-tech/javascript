6f4b00d9a923517feb3a00d5d9aa20328d282dfe
ritikrahul57-tech <ritikrahul57@gmail.com>
2026-08-16 20:55:06 +0530

    The javascript day10

--- section3.js
+++ section3.js
@@ -2,7 +2,7 @@
 
 
 
-//         // Type conversion
+//              // Type conversion
 
 
 
@@ -15,8 +15,80 @@
 
 
 // 2.    type casting
+                //  WE will learn in the future
+
+
+// 3.     string converstion
+
+                //   let age=78;
+                //   console.log(typeof age) 
+                //   age=String(age)
+                //   console.log(typeof age)
+
+                //   let rahul=true
+                //   rahul=String(rahul)
+                //   console.log(typeof rahul)
+ 
+
+//4.      number converstion
+
+                // let fah='22'
+                // console.log(typeof fah)
+                // fah=Number(fah)
+                // console.log(fah+4)
+
+                // let fah=true;      The value will be 1
+                // let ooo=false;     The value will be 0
+                // let ppp='true'      If the value is given in string then the output will be not a number(nan)
+                // fah=Number(fah)
+                // console.log(fah)
+
+                // let fah='123k'       The answer is not a number
+                // fah=Number(fah)
+                // console.log(typeof fah)
+
+                // let fah=undefined;    The output is not a number
+                // fah=Number(fah)
+                // console.log(fah)
+
+
+//5.    boolean convestion
+
+
+                // let z=5;      Any value is given in the boolean value will be true
+                // let y=0;      only when the value is 0 . the output will be false
+                // z=Boolean(z)
+                // console.log(typeof z)
+
+                // let z='false'    Any word or any number is given in the string will be true
+                // let y=''         only when the string is in empty
+                // z=Boolean(z)
+                // console.log(z)
 
 
 
 
-ppppooooiii
+
+                        //      OPERATOR
+
+
+ 
+// 4+9=13
+// + is a operator
+// 4,9 are operands
+
+
+
+        //   Type of operands
+                    /* -unary
+                       -
+                    *\
+
+
+
+// 1.  unary
+
+
+
+ 
+
