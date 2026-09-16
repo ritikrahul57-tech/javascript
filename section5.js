3c3506fe9b8dc6465cbd8212984e86d695f16a74
ritikrahul57-tech <ritikrahul57@gmail.com>
2026-09-16 21:28:51 +0530

    The javascript day20

---
+++ section5.js
@@ -0,0 +1,15 @@
+//                 conditional operator
+
+
+
+// let person;
+// let age=18;
+// person=(age>17)? 'adult' : 'child';
+// console.log(person)
+
+
+
+// let person;
+// let age=18;
+// person=(age>17)? 'adult' : (age>12) 'child';
+// console.log(person)
