766477355a164dbb5d846011947efc8738776ded
ritikrahul57-tech <ritikrahul57@gmail.com>
2026-09-16 22:01:37 +0530

    The javascript day20

--- section5.js
+++ section5.js
@@ -11,5 +11,66 @@
 
 // let person;
 // let age=18;
-// person=(age>17)? 'adult' : (age>12) 'child';
+// person=(age>17)? 'adult' : (age>12) 'child' : 'smaller child';
 // console.log(person)
+
+
+// if('0'){
+//     console.log('hi')
+// }
+
+
+//              num case function
+
+
+
+// let x=10;
+// if(x>0){
+//     console.log('positive')
+// }
+// else if(x<0){
+//     console.log('negative')
+// }
+// else{
+//     console.log('zero')
+// }
+
+
+
+// let x=10;
+// if(x>0){
+//     console.log('positive')
+// }
+// if(x<0){
+//     console.log('negative')
+// }
+// if{
+//     console.log('zero')
+// }
+
+
+//   THE CONDITION FOR LEAP YEAR
+//THE NUMBER SHOULD DIVISIBLE BY 4,
+//AND THE NUMBER SHOULD DIVISIBLE BY 100? , DIVISIBLE BY 400:
+
+// let year=2000;
+// if(year%4==0){
+//     if(year%100==0){
+//         if(year%400==0){
+//             console.log("leap year")
+//         }
+//         else{
+//             console.log('not a leap year')
+//         }
+//     }
+//     else{
+//         console.log('leap year')
+//     }
+// }
+// else{
+//     console.log('not a leap year')
+// }
+
+
+
+
